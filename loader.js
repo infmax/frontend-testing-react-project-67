d0c97532740c2b5b1265ec1714aa8f8b2d11062e
Махмутов Рустем Дмитриевич <r.mahmutov@phoenixit.ru>
2023-02-26 15:26:06 +0300

    linter

--- loader.js
+++ loader.js
@@ -1,100 +1,99 @@
 #!/usr/bin/env node
 
-import fs from 'fs/promises'
-import fsA from 'fs'
-import Debug from 'debug'
-import * as cheerio from 'cheerio'
-import axios from 'axios'
-import 'axios-debug-log'
-import path from 'path'
+/* eslint-disable no-await-in-loop, no-restricted-syntax, no-continue */
 
-const debug = Debug('page-loader')
+import fs from 'fs/promises';
+import fsA from 'fs';
+import Debug from 'debug';
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+import 'axios-debug-log';
+import path from 'path';
+
+const debug = Debug('page-loader');
 
 const load = async (url, dir = '.') => {
-  if (!url || !url.toString() || typeof url !== "string") {
-    throw new Error('Empty url')
+  if (!url || !url.toString() || typeof url !== 'string') {
+    throw new Error('Empty url');
   }
 
-  const fileName = url.replace(/http[s]?:\/\//g, '').replace(/[^0-9a-zA-Z]/g, '-')
+  const fileName = url.replace(/http[s]?:\/\//g, '').replace(/[^0-9a-zA-Z]/g, '-');
 
-  let response
+  let mainResponse;
 
   try {
-    response = await axios.get(url)
-  }
-  catch (e) {
-    debug('Url load problem: ', e)
+    mainResponse = await axios.get(url);
+  } catch (e) {
+    debug('Url load problem: ', e);
 
-    throw e
+    throw e;
   }
   // debug(response)
 
-  const content = response.data
+  const content = mainResponse.data;
 
-  const $ = cheerio.load(content)
+  const $ = cheerio.load(content);
 
-  const origin = (new URL(url)).origin
+  const { origin } = new URL(url);
 
-  const tags = $('img,link,script').map(function () {
+  function mapTags() {
+    const srcUrl = $(this).attr('src') || $(this).attr('href');
+    const src = new URL(srcUrl, origin);
 
-    const url = $(this).attr('src') || $(this).attr('href')
-    const src = new URL(url, origin)
+    return { element: $(this), src, originalSrc: srcUrl };
+  }
 
-    return {element: $(this), src, originalSrc: url}
-  })
+  const tags = $('img,link,script').map(mapTags);
 
   if (fsA.existsSync(`${dir}/${fileName}_files`)) {
-    await fs.rmdir(`${dir}/${fileName}_files`, {recursive: true})
+    await fs.rmdir(`${dir}/${fileName}_files`, { recursive: true });
   }
-  await fs.mkdir(`${dir}/${fileName}_files`)
+  await fs.mkdir(`${dir}/${fileName}_files`);
 
-  for (let tag of tags) {
-    const tagName = tag.element[0].name
-    const file = tag.src.toString()
+  for (const tag of tags) {
+    const tagName = tag.element[0].name;
+    const file = tag.src.toString();
 
     if (!file || tag.src.origin !== origin) {
-      continue
+      continue;
     }
 
-    let response
+    let response;
 
     try {
-      response = await axios.get(file, {responseType: 'arraybuffer'})
-    }
-    catch (e) {
-      debug('Static load error: ', file, e)
+      response = await axios.get(file, { responseType: 'arraybuffer' });
+    } catch (e) {
+      debug('Static load error: ', file, e);
     }
 
     if (!response || !response.data) {
-      debug('Static load error: ', file)
-      continue
+      debug('Static load error: ', file);
+      continue;
     }
 
-    const { dir: fileDir, name, ext } = path.parse(file)
+    const { dir: fileDir, name, ext } = path.parse(file);
 
     const staticFileName = path.join(fileDir, name).replace(/http[s]?:\//g, '')
-        .replace(/[^0-9a-zA-Z]/g, '-')
-    const rewriteName = `${fileName}_files/${staticFileName}${ext || '.html' }`
+      .replace(/[^0-9a-zA-Z]/g, '-');
+    const rewriteName = `${fileName}_files/${staticFileName}${ext || '.html'}`;
 
     if (tagName !== 'link') {
-      $(`${tagName}[src="${tag.originalSrc}"]`).attr('src', rewriteName)
-    }
-    else {
-      $(`${tagName}[href="${tag.originalSrc}"]`).attr('href', rewriteName)
+      $(`${tagName}[src="${tag.originalSrc}"]`).attr('src', rewriteName);
+    } else {
+      $(`${tagName}[href="${tag.originalSrc}"]`).attr('href', rewriteName);
     }
 
     try {
-      await fs.writeFile(`${dir}/${rewriteName}`, response.data)
-    }
-    catch (e) {
-      debug('File write error: ', file, e)
+      await fs.writeFile(`${dir}/${rewriteName}`, response.data);
+    } catch (e) {
+      debug('File write error: ', file, e);
     }
   }
 
-  await fs.writeFile(`${dir}/${fileName}.html`, $.html())
+  await fs.writeFile(`${dir}/${fileName}.html`, $.html());
 
-  return { filepath: `${dir}/${fileName}.html` }
-}
+  return { filepath: `${dir}/${fileName}.html` };
+};
 
-export default load
+export default load;
 // console.log(args)
--- page-loader.js
+++ page-loader.js
@@ -1,18 +1,18 @@
-import parseArgs from 'minimist'
-import process from 'process'
-import load from './loader.js'
+import parseArgs from 'minimist';
+import process from 'process';
+import load from './loader.js';
 
-const args = parseArgs(process.argv.slice(2))
+const args = parseArgs(process.argv.slice(2));
 
-const dir = args.output || args.o || process.cwd()
+const dir = args.output || args.o || process.cwd();
 
-const url = args._[0]
+const url = args._[0];
 
 load(url, dir)
-    .then(({filepath}) => {
-      console.log(filepath)
-    })
-    .catch((err) => {
-      console.error(err.toString())
-      process.exit(1)
-    })
+  .then(({ filepath }) => {
+    console.log(filepath);
+  })
+  .catch((err) => {
+    console.error(err.toString());
+    process.exit(1);
+  });
