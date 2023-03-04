6022c9075b7c7f83eadf44093541bcbacdf01642
Махмутов Рустем Дмитриевич <r.mahmutov@phoenixit.ru>
2023-03-04 18:29:52 +0300

    2 describes

--- __tests__/loader.test.js
+++ __tests__/loader.test.js
@@ -5,77 +5,53 @@ import fsA from 'fs';
 import os from 'os';
 import load from '../loader.js';
 
-describe('page loader', () => {
-  let html;
+let html;
 
-  let dir;
-  let loadedHtml;
-  let wrongHtml;
-  let css;
+let dir;
+let loadedHtml;
+let wrongHtml;
+let css;
 
-  nock.disableNetConnect()
+nock.disableNetConnect()
 
-  beforeAll(async () => {
-    html = (await fs.readFile(`${__dirname}/__fixtures__/page.html`, 'utf-8')).trim();
-    css = (await fs.readFile(`${__dirname}/__fixtures__/style.css`, 'utf-8')).trim();
+beforeAll(async () => {
+  html = (await fs.readFile(`${__dirname}/__fixtures__/page.html`, 'utf-8')).trim();
+  css = (await fs.readFile(`${__dirname}/__fixtures__/style.css`, 'utf-8')).trim();
 
-    html = (await fs.readFile(`${__dirname}/__fixtures__/page.html`, 'utf-8')).trim();
-    loadedHtml = (await fs.readFile(`${__dirname}/__fixtures__/loaded-page.html`, 'utf-8')).trim();
-    wrongHtml = (await fs.readFile(`${__dirname}/__fixtures__/wrong-page.html`, 'utf-8')).trim();
-  });
+  html = (await fs.readFile(`${__dirname}/__fixtures__/page.html`, 'utf-8')).trim();
+  loadedHtml = (await fs.readFile(`${__dirname}/__fixtures__/loaded-page.html`, 'utf-8')).trim();
+  wrongHtml = (await fs.readFile(`${__dirname}/__fixtures__/wrong-page.html`, 'utf-8')).trim();
+});
 
-  beforeEach(async () => {
-    nock('https://google.com')
+beforeEach(async () => {
+  nock('https://google.com')
       .get('/')
       .reply(200, html);
 
-    nock('https://google.com')
+  nock('https://google.com')
       .get('/courses')
       .reply(200, html);
 
-    nock('https://google.com')
+  nock('https://google.com')
       .get('/assets/style.css')
       .reply(200, css);
 
-    nock('https://google.com')
-        .get('/assets/example.jpg')
-        .reply(200, 'any');
+  nock('https://google.com')
+      .get('/assets/example.jpg')
+      .reply(200, 'any');
 
-    nock('https://google.com')
+  nock('https://google.com')
       .get('/assets/runtime.js')
       .reply(200, 'var a = 0');
 
-    nock('https://google-wrong.com')
+  nock('https://google-wrong.com')
       .get('/')
       .reply(200, wrongHtml);
 
-    dir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
-  });
+  dir = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+});
 
-  it('failed with empty url', async () => {
-    await expect(load('', dir)).rejects.toThrow();
-  });
-
-  it('failed with object url', async () => {
-    await expect(load({}, dir)).rejects.toThrow();
-  });
-
-  it('failed with wrong url', async () => {
-    await expect(load('https://atatat/atata', dir)).rejects.toThrow();
-  });
-
-  it('failed with wrong img', async () => {
-    await expect(load('https://atatat/atata', dir)).rejects.toThrow();
-  });
-
-  it('failed with wrong img src', async () => {
-    await load('https://google-wrong.com', dir);
-
-    const loadedContent = (await fs.readFile(`${dir}/google-wrong-com.html`, 'utf-8')).trim();
-
-    expect(loadedContent).toBe(wrongHtml);
-  });
-
+describe('positive scenarios', () => {
   it('matched html content', async () => {
     await load('https://google.com', dir);
 
@@ -120,5 +96,31 @@ describe('page loader', () => {
   it('created html', async () => {
     await load('https://google.com', dir);
     expect(fsA.existsSync(`${dir}/google-com_files/google-com-courses.html`)).toBe(true);
+  });
+});
+
+describe('negative scenarios', () => {
+  it('failed with empty url', async () => {
+    await expect(load('', dir)).rejects.toThrow();
+  });
+
+  it('failed with object url', async () => {
+    await expect(load({}, dir)).rejects.toThrow();
+  });
+
+  it('failed with wrong url', async () => {
+    await expect(load('https://atatat/atata', dir)).rejects.toThrow();
+  });
+
+  it('failed with wrong img', async () => {
+    await expect(load('https://atatat/atata', dir)).rejects.toThrow();
+  });
+
+  it('failed with wrong img src', async () => {
+    await load('https://google-wrong.com', dir);
+
+    const loadedContent = (await fs.readFile(`${dir}/google-wrong-com.html`, 'utf-8')).trim();
+
+    expect(loadedContent).toBe(wrongHtml);
   });
 });
