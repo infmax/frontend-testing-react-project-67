82f21cc6993a18c7485a65c22b8cc8d9ede6d6cf
Махмутов Рустем Дмитриевич <r.mahmutov@phoenixit.ru>
2023-03-04 18:22:49 +0300

    commander

---
+++ bin/page-loader.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+/* linebreak-style: ["error", "windows"] */
+
+import { program } from 'commander';
+import process from 'process';
+import pageLoader from '../loader.js';
+
+program
+  .command('page-loader')
+  .version('1.0.0')
+  .description('Page download')
+  .arguments('<url>', 'url source')
+  .option('-o, --output [dirPath]', 'output directory', process.cwd())
+  .action(async (url, options) => {
+    console.log(await pageLoader(url, options.output));
+  })
+  .parse(process.argv);
